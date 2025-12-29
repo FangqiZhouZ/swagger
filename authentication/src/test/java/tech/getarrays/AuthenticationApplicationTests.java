4317ba6cdd50a54fd557687c29a59e4c6cfba6c1
FangqiZhouZ <fangqizhou@gmail.com>
2025-12-29 17:32:08 +0100

    docker compose for authentication and db

--- authentication/src/test/java/tech/getarrays/AuthenticationApplicationTests.java
+++ authentication/src/test/java/tech/getarrays/AuthenticationApplicationTests.java
@@ -1,16 +1,16 @@
 package tech.getarrays;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test; // JUnit 5
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// 删掉这行导入: import org.junit.runner.RunWith;
+// 删掉这行导入: import org.springframework.test.context.junit4.SpringRunner;
+
+// 删掉这个注解: @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthenticationApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
